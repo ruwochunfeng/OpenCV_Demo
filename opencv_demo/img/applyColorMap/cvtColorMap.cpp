38df98912a12b1c3650b0d4094c3cd0003e0b37c
ruwochunfeng <2267136614@qq.com>
2021-02-17 11:26:33 +0800

    add applyColorMap

---
+++ opencv_demo/img/applyColorMap/cvtColorMap.cpp
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <opencv2/opencv.hpp>
+
+using namespace cv;
+
+int main(int argc, char** argv)
+{
+    if(argc != 2)
+    {
+       printf("Opencv first Demo");
+       return -1;
+
+    }
+    
+    Mat image;
+    image = imread( argv[1],1);
+    
+    if(!image.data)
+    {
+       printf("No image data \n");
+       return -1;
+    }
+    
+    applyColorMap(image,image, COLORMAP_AUTUMN);
+    namedWindow("Display Image", WINDOW_AUTOSIZE);
+    imshow("Display Image", image);
+    
+    waitKey(0);
+    
+    return 0;
+}
+
